1b700001e6333126903042cf64789fbdc6482582
cesarmaya1006 <cesarmaya1006@gmail.com>
2021-04-05 21:55:33 -0500

    Cm-05-04-2021 MIgracion con menus

--- database/seeders/Tabla_MenuRol.php
+++ database/seeders/Tabla_MenuRol.php
@@ -14,9 +14,21 @@ class Tabla_MenuRol extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 8; $i++) {
+        for ($i = 1; $i <= 12; $i++) {
             DB::table('menu_rol')->insert([
                 'rol_id' => '1',
+                'menu_id' => $i,
+            ]);
+        }
+        for ($i = 2; $i <= 6; $i++) {
+            DB::table('menu_rol')->insert([
+                'rol_id' => $i,
+                'menu_id' => '1',
+            ]);
+        }
+        for ($i = 9; $i <= 12; $i++) {
+            DB::table('menu_rol')->insert([
+                'rol_id' => '6',
                 'menu_id' => $i,
             ]);
         }
