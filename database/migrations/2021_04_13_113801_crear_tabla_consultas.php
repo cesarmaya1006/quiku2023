94f24620cf839b27fb1f28abcd991ab7a22d25de
cesarmaya1006 <cesarmaya1006@gmail.com>
2021-04-13 12:45:04 -0500

    Cm - 13-04-2021 Ajuste de Menus y Roles

--- database/migrations/2021_04_13_113801_crear_tabla_consultas.php
+++ database/migrations/2021_04_13_113801_crear_tabla_consultas.php
@@ -14,8 +14,22 @@ class CrearTablaConsultas extends Migration
     public function up()
     {
         Schema::create('consultas', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id')->autoIncrement();
+            $table->unsignedBigInteger('persona_id')->nullable();
+            $table->foreign('persona_id', 'fk_persona_pqr')->references('id')->on('personas')->onDelete('restrict')->onUpdate('restrict');
+            $table->unsignedBigInteger('empresa_id')->nullable();
+            $table->foreign('empresa_id', 'fk_empresa_pqr')->references('id')->on('empresas')->onDelete('restrict')->onUpdate('restrict');
+            $table->unsignedBigInteger('empleado_id')->nullable();
+            $table->foreign('empleado_id', 'fk_empleado_pqr')->references('id')->on('empleados')->onDelete('restrict')->onUpdate('restrict');
+            $table->text('consulta');
+            $table->text('justificacion');
+            $table->boolean('prorroga')->default(0)->nullable();
+            $table->date('fecha_radicado');
+            $table->date('fecha_respuesta')->nullable();
+            $table->string('estado')->default('Radicada, sin iniciar tramite');
             $table->timestamps();
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_spanish_ci';
         });
     }
 
