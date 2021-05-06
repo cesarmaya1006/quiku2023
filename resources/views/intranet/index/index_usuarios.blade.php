25613740b23de7ed99327f67e72ab243760b8280
cesarmaya1006 <cesarmaya1006@gmail.com>
2021-05-05 20:26:50 -0500

    Cm cambios  finales

--- resources/views/intranet/index/index_usuarios.blade.php
+++ resources/views/intranet/index/index_usuarios.blade.php
@@ -1,54 +1,141 @@
-<div class="row">
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-info">
-            <div class="inner">
-                <h3>{{ $pqr_S->where('tipo', 'peticion')->count() }}</h3>
-                <p>Peticiones</p>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $pqr_S->count() }}</h3>
+                    <p>Peticiones,Quejas,Reclamos</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-            <div class="icon">
-                <i class="ion ion-bag"></i>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $conceptos->count() }}</h3>
+
+                    <p>Consultas</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-            <a href="#" class="small-box-footer">Ver <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $solicitudes_datos->count() }}</h3>
+
+                    <p>Solicitud de datos personales</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $denuncias->count() }}</h3>
+
+                    <p>Denuncias</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-pink">
+                <div class="inner">
+                    <h3>{{ $felicitaciones->count() }}</h3>
+
+                    <p>Felicitaciones</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-teal">
+                <div class="inner">
+                    <h3>{{ $solicitudes_doc->count() }}</h3>
+
+                    <p>Solicitud de documentos o información</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-ingo"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-indigo">
+                <div class="inner">
+                    <h3>{{ $sugerencias->count() }}</h3>
+
+                    <p>Sugerencias</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-sm table-hover display">
+                <thead>
+                    <tr>
+                        <th>Num Radicado</th>
+                        <th>Tipo de PQR</th>
+                        <th>Fecha de Radicado</th>
+                        <th>Estado</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($pqr_S as $pqr)
+                    @if ($pqr_S->where('estado',!=,'cerrada'))
+                    <?php $$fec_radicado =date("d-m-Y",strtotime($pqr->."+ 1 days")); ?>
+                    <tr>
+                        <td>{{$pqr->radicado}}</td>
+                        <td>{{$pqr->tipoPqr->tipo}}</td>
+                        <td>{{$pqr->fecha_radicado}}</td>
+                        <td>{{$pqr->radicado}}</td>
+                        <td>{{$pqr->radicado}}</td>
+                    </tr>
+                    @endif
+                    @endforeach
+                    
+                </tbody>
+            </table>
         </div>
     </div>
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-orange">
-            <div class="inner">
-                <h3>{{ $pqr_S->where('tipo', 'queja')->count() }}</h3>
-                <p>Quejas</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">Ver <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-teal">
-            <div class="inner">
-                <h3>{{ $pqr_S->where('tipo', 'reclamo')->count() }}</h3>
-                <p>Reclamos</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">Ver <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-purple">
-            <div class="inner">
-                <h3>{{ $pqr_S->count() }}</h3>
-                <p>PQR´s</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">Ver <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
+
 </div>
