4f3220d89d69297cd3ad59908fb1b3ccf99994da
wpruiz <ruizwilson01@gmail.com>
2021-06-07 14:06:51 -0500

    WR 07-06-2021 configuraciones heroku

--- database/seeders/Tabla_MenuRol.php
+++ database/seeders/Tabla_MenuRol.php
@@ -14,29 +14,29 @@ class Tabla_MenuRol extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 12; $i++) {
-            DB::table('menu_rol')->insert([
-                'rol_id' => '1',
-                'menu_id' => $i,
-            ]);
-        }
-        for ($i = 2; $i <= 6; $i++) {
-            DB::table('menu_rol')->insert([
-                'rol_id' => $i,
-                'menu_id' => '1',
-            ]);
-        }
-        for ($i = 9; $i <= 15; $i++) {
-            DB::table('menu_rol')->insert([
-                'rol_id' => '6',
-                'menu_id' => $i,
-            ]);
-        }
-        for ($i = 16; $i <= 19; $i++) {
-            DB::table('menu_rol')->insert([
-                'rol_id' => '5',
-                'menu_id' => $i,
-            ]);
-        }
+        // for ($i = 1; $i <= 12; $i++) {
+        //     DB::table('menu_rol')->insert([
+        //         'rol_id' => '1',
+        //         'menu_id' => $i,
+        //     ]);
+        // }
+        // for ($i = 2; $i <= 6; $i++) {
+        //     DB::table('menu_rol')->insert([
+        //         'rol_id' => $i,
+        //         'menu_id' => '1',
+        //     ]);
+        // }
+        // for ($i = 9; $i <= 15; $i++) {
+        //     DB::table('menu_rol')->insert([
+        //         'rol_id' => '6',
+        //         'menu_id' => $i,
+        //     ]);
+        // }
+        // for ($i = 16; $i <= 19; $i++) {
+        //     DB::table('menu_rol')->insert([
+        //         'rol_id' => '5',
+        //         'menu_id' => $i,
+        //     ]);
+        // }
     }
 }
