1b6da30fe5fc6ab2abcc72df3f6230856928244a
wpruiz <ruizwilson01@gmail.com>
2021-06-07 14:20:40 -0500

    WR 07-06-2021 configuraciones heroku

--- database/seeders/Tabla_MenuRol.php
+++ database/seeders/Tabla_MenuRol.php
@@ -1,42 +1,42 @@
 <?php
 
-// namespace Database\Seeders;
+namespace Database\Seeders;
 
-// use Illuminate\Database\Seeder;
-// use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-// class Tabla_MenuRol extends Seeder
-// {
-//     /**
-//      * Run the database seeds.
-//      *
-//      * @return void
-//      */
-//     public function run()
-//     {
-//         for ($i = 1; $i <= 12; $i++) {
-//             DB::table('menu_rol')->insert([
-//                 'rol_id' => '1',
-//                 'menu_id' => $i,
-//             ]);
-//         }
-//         for ($i = 2; $i <= 6; $i++) {
-//             DB::table('menu_rol')->insert([
-//                 'rol_id' => $i,
-//                 'menu_id' => '1',
-//             ]);
-//         }
-//         for ($i = 9; $i <= 15; $i++) {
-//             DB::table('menu_rol')->insert([
-//                 'rol_id' => '6',
-//                 'menu_id' => $i,
-//             ]);
-//         }
-//         for ($i = 16; $i <= 19; $i++) {
-//             DB::table('menu_rol')->insert([
-//                 'rol_id' => '5',
-//                 'menu_id' => $i,
-//             ]);
-//         }
-//     }
-// }
+class Tabla_MenuRol extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 12; $i++) {
+            DB::table('menu_rol')->insert([
+                'rol_id' => '1',
+                'menu_id' => $i,
+            ]);
+        }
+        for ($i = 2; $i <= 6; $i++) {
+            DB::table('menu_rol')->insert([
+                'rol_id' => $i,
+                'menu_id' => '1',
+            ]);
+        }
+        for ($i = 9; $i <= 15; $i++) {
+            DB::table('menu_rol')->insert([
+                'rol_id' => '6',
+                'menu_id' => $i,
+            ]);
+        }
+        for ($i = 16; $i <= 19; $i++) {
+            DB::table('menu_rol')->insert([
+                'rol_id' => '5',
+                'menu_id' => $i,
+            ]);
+        }
+    }
+}
