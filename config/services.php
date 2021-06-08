535ae4f2d234341638385b434fba45be40ac8361
wpruiz <ruizwilson01@gmail.com>
2021-06-07 19:22:05 -0500

    WR 07-06-2021 ajustes heroku email

--- config/services.php
+++ config/services.php
@@ -16,7 +16,8 @@ return [
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET')
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
     'postmark' => [
